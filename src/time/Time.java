d2d0d25d03aa355f42202ee4ec115d254b45388e
APRENDIZ <domain1@domain1.com>
2017-05-05 09:43:44 -0500

    Version 0.2

--- src/time/Time.java
+++ src/time/Time.java
@@ -10,5 +10,43 @@ package time;
  * @author APRENDIZ
  */
 public class Time {
-    
+
+    private int minute;
+    private int m;
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    private static void delaySecond() {
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("error delay: ");
+        }
+
+    }
+
+    public void timer() {
+
+         System.out.println("TIEMPO");
+        for (int i = minute; i >= 0; i--) {
+
+            for (int second =60; second >= 0; second --) {
+
+                System.out.println(i-1 + " : " + second);
+                delaySecond();
+
+            }
+           // System.out.println(i);
+        }
+
+        System.out.println("RINGGG!! Ya han transcurrido " + m + " minutos");
+
+    }
 }
