3da97b15976041fc21ac88d791407d6ee6f58828
cacastano95 <cacastano95@misena.edu.co>
2017-05-05 11:09:49 -0500

    Version 0.1

--- src/time/Time.java
+++ src/time/Time.java
@@ -5,10 +5,56 @@
  */
 package time;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author APRENDIZ
  */
 public class Time {
     
-}
+    
+    public static void deley(){
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("Error delay: " + e);
+        }
+        
+    }
+    
+    public void alarma(){
+    
+        Scanner sc = new Scanner (System.in);
+        
+             
+        System.out.println("-------ALARMA-------");
+        System.out.print("En cuantas horas desea que te despierte: ");
+        int hora = sc.nextInt();
+
+        for (int i = 0; i < hora; i++) {
+            
+           
+                            
+            for (int j = 0; j < 60; j++) {
+              
+                for (int k = 0; k < 60; k++) {
+                    
+                
+                 System.out.println(i+ ":" + j + ":" + k );
+                 Time.deley();
+                  
+                 
+                }
+            }
+            }
+        
+        System.out.println("!!Despierta!!");
+            
+        }
+       
+}   
